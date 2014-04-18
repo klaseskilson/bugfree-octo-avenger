aa1b5cbd9d16830108533349135b4df8f5c6724e
Klas Eskilson <klas.eskilson@gmail.com>
2014-04-19 00:13:01 +0200

    Added new default data and form reset function

--- script/main.js
+++ script/main.js
@@ -1,13 +1,29 @@
 angular.module('bugfree', [])
 .controller('Result', function ($scope) {
     $scope.entries =
-        [{first_name:'John', last_name:'Klasson', time:{hh: 1, mm: 1, ss: 1}},
-         {first_name:'Mary', last_name:'Klassson', time:{hh: 2, mm: 2, ss: 2}},
-         {first_name:'Mike', last_name:'Klfasson', time:{hh: 7, mm: 3, ss: 0}},
-         {first_name:'Adam', last_name:'Klassöon', time:{hh: 3, mm: 7, ss: 0}},
-         {first_name:'Julie', last_name:'Öl', time:{hh: 0, mm: 55, ss: 7}}]
+        [{first_name:'Magnus', last_name:'Hult', time:{hh: 1, mm: 1, ss: 1}},
+         {first_name:'Arvid', last_name:'Janson', time:{hh: 2, mm: 2, ss: 2}},
+         {first_name:'Louise', last_name:'Eriksson', time:{hh: 7, mm: 3, ss: 0}},
+         {first_name:'Leo', last_name:'Giertz', time:{hh: 3, mm: 7, ss: 45}},
+         {first_name:'Stefan', last_name:'Granlund', time:{hh: 1, mm: 7, ss: 0}},
+         {first_name:'Edvard', last_name:'Backendguru', time:{hh: 3, mm: 7, ss: 22}},
+         {first_name:'Klas', last_name:'Eskilson', time:{hh: 0, mm: 7, ss: 56}},
+         {first_name:'Linnéa', last_name:'Nåbo', time:{hh: 3, mm: 7, ss: 5}},
+         {first_name:'Lovisa', last_name:'Dahl', time:{hh: 0, mm: 55, ss: 7}}]
     $scope.predicate = 'time';
     $scope.reverse = false;
+
+    $scope.reset = function () {
+        // emtpy fields
+        $scope.first_name = '';
+        $scope.last_name = '';
+        $scope.time_hh = '';
+        $scope.time_mm = '';
+        $scope.time_ss = '';
+
+        // reset form inputs to pristine state
+        $scope.entryForm.$setPristine();
+    };
 
     $scope.addEntry = function () {
         // create new entry
@@ -20,16 +36,12 @@ angular.module('bugfree', [])
                 ss: $scope.time_ss
             }
         };
-        {
-            // push to entries
-            $scope.entries.push(obj);
-            // emtpy fields
-            $scope.first_name = '';
-            $scope.last_name = '';
-            $scope.time_hh = '';
-            $scope.time_mm = '';
-            $scope.time_ss = '';
-        }
+
+        // push to entries
+        $scope.entries.push(obj);
+
+        // reset form
+        $scope.reset();
     };
 })
 .filter('prettyTime', function () {
@@ -39,6 +51,6 @@ angular.module('bugfree', [])
             mm = n.mm,
             ss = n.ss;
 
-        return (hh) + ':' + (parseInt(mm, 10) > 9 ? mm : '0' + mm) + ':' + (parseInt(ss, 10) > 9 ? ss : '0' + ss);
+        return (hh) + ':' + (parseInt(mm) > 9 ? mm : '0' + mm) + '.' + (parseInt(ss) > 9 ? ss : '0' + ss);
     };
 });;
