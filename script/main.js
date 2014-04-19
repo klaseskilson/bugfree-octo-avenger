3280642e9f24c37b27d8f635c81ea8cdd493aeea
Klas Eskilson <klas.eskilson@gmail.com>
2014-04-19 13:18:11 +0200

    Added some comments

--- script/main.js
+++ script/main.js
@@ -1,35 +1,35 @@
 angular.module('bugfree', [])
     .controller('Result', function ($scope) {
+        // add dummy data
         $scope.entries =
-            [{first_name:'Magnus', last_name:'Hult', time:{hh: 1, mm: 1, ss: 1}},
-             {first_name:'Arvid', last_name:'Janson', time:{hh: 2, mm: 2, ss: 2}},
-             {first_name:'Louise', last_name:'Eriksson', time:{hh: 7, mm: 3, ss: 0}},
-             {first_name:'Leo', last_name:'Giertz', time:{hh: 3, mm: 7, ss: 45}},
-             {first_name:'Stefan', last_name:'Granlund', time:{hh: 1, mm: 7, ss: 0}},
-             {first_name:'Edvard', last_name:'Backendguru', time:{hh: 3, mm: 7, ss: 22}},
-             {first_name:'Klas', last_name:'Eskilson', time:{hh: 0, mm: 7, ss: 56}},
-             {first_name:'Linnéa', last_name:'Nåbo', time:{hh: 3, mm: 7, ss: 5}},
-             {first_name:'Lovisa', last_name:'Dahl', time:{hh: 0, mm: 55, ss: 7}}]
+            [{first_name:'Granny',  last_name:'Smith',  time:{hh: 1, mm: 1, ss: 1}},
+             {first_name:'Rubi',    last_name:'Nola',   time:{hh: 2, mm: 2, ss: 2}},
+             {first_name:'Jona',    last_name:'Gold',   time:{hh: 7, mm: 3, ss: 0}},
+             {first_name:'Mac',     last_name:'Intosh', time:{hh: 3, mm: 7, ss: 45}}]
+        // default sorting!
         $scope.predicate = 'time';
         $scope.reverse = false;
 
         $scope.reset = function () {
             // emtpy fields
             $scope.first_name = '';
-            $scope.last_name = '';
-            $scope.time_hh = '';
-            $scope.time_mm = '';
-            $scope.time_ss = '';
+            $scope.last_name  = '';
+            $scope.time_hh    = '';
+            $scope.time_mm    = '';
+            $scope.time_ss    = '';
 
-            // reset form inputs to pristine state
+            // reset form to pristine state
             $scope.entryForm.$setPristine();
         };
 
+        /**
+         * add new entry to the entries array
+         */
         $scope.addEntry = function () {
-            // create new entry
-            var obj = {
+            // create new entry, get form inputs from ng-model
+            var entry = {
                 first_name: $scope.first_name,
-                last_name: $scope.last_name,
+                last_name:  $scope.last_name,
                 time: {
                     hh: $scope.time_hh,
                     mm: $scope.time_mm,
@@ -38,18 +38,22 @@ angular.module('bugfree', [])
             };
 
             // push to entries
-            $scope.entries.push(obj);
+            $scope.entries.push(entry);
 
             // reset form
             $scope.reset();
         };
     })
+    /**
+     * display time in a neat way, apply on the time object
+     * @return {string} the nice time
+     */
     .filter('prettyTime', function () {
-        // not sure filter is the best way to do this
-        return function (n) {
-            var hh = n.hh,
-                mm = n.mm,
-                ss = n.ss;
+        // not sure filter is the best way to do this, but it does the job!
+        return function (time) {
+            var hh = time.hh,
+                mm = time.mm,
+                ss = time.ss;
 
             return (hh) + ':' + (parseInt(mm) > 9 ? mm : '0' + mm) + '.' + (parseInt(ss) > 9 ? ss : '0' + ss);
         };
